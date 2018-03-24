94f405797f0689befb7a629ce98e4057feecbe56
Invigo <invigo.devel@gmail.com>
2018-03-24 16:34:06 +0200

    added tests

--- src/test/frenchdeck_test.py
+++ src/test/frenchdeck_test.py
@@ -1,19 +1,36 @@
-import unittest
-from main.frenchdeck import *
+import unittest, sys
+
+sys.path.extend(["."])
+
+from src.main.frenchdeck import FrenchDeck, Card
 
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.beer_card = ('7', 'diamonds')
+        self.beer_card = Card('7', 'diamonds')
+        self.ace_hearts = Card('A', 'hearts')
         self.deck = FrenchDeck()
 
-    def test_something(self):
+    def test_beer_card(self):
         self.setUp()
         self.assertEqual(self.beer_card.rank, '7')
         self.assertIs(self.beer_card.suit, 'diamonds')
+
+    def test_ace_hearts(self):
+        self.assertEqual(self.ace_hearts.rank, 'A')
+        self.assertIs(self.ace_hearts.suit, 'hearts')
+
+    def test_instances(self):
+        self.setUp()
         self.assertIsInstance(self.deck, FrenchDeck)
-        self.assertEquals(len(self.deck), 52)
+        self.assertIsInstance(self.beer_card, Card)
+
+    def test_deck(self):
+        self.assertEqual(len(self.deck), 52)
+
+    # def test_high(self):
+    #     self.assertGreater(hearts_high(self.ace_hearts), hearts_high(self.beer_card))
 
     def tearDown(self):
         pass
